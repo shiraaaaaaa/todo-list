f7c38a1914bbe1655229d5460a81310c9c2f37f8
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2024-12-09 16:19:42 +0200

    Add formik

--- src/components/TagsInput.tsx
+++ src/components/TagsInput.tsx
@@ -1,7 +1,9 @@
 import { KeyboardEvent, useState } from 'react'
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, styled, TextField } from '@mui/material';
 
 import CloseIcon from '@mui/icons-material/Close';
+import { useFormikContext } from 'formik';
+import { Task } from '../types/task';
 
 export interface TagsInputProps {
     tags: string[];
@@ -9,46 +11,39 @@ export interface TagsInputProps {
     name: string
 }
 
+const SubjectButton = styled(Button)(() => ({
+    margin: "1px",
+    fontSize: "10px",
+    padding: "4px",
+}));
 
-const TagsInput = ({ tags, setTags }: TagsInputProps) => {
+
+const TagsInput = () => {
+    const { values, setFieldValue } = useFormikContext<Task>();
     const [text, setText] = useState('');
 
     const handleRemoveTag = (index: number) => {
-        setTags([...tags.filter((_tag, i) => i !== index)])
+        setFieldValue("subjects", [...values.subjects.filter((_tag, i) => i !== index)])
     }
 
     const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
         if (e.key === 'Enter' && text.trim() !== '') {
-            setTags([...tags, text.trim()]);
+            setFieldValue("subjects", [...values.subjects, text.trim()]);
             setText('');
         }
     };
 
     return (
         <>
-            <TextField
-                type="text"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                onKeyDown={handleKeyDown}
-                placeholder="Type and press Enter"
-
-            />
+            <TextField type="text" value={text} onChange={(e) => setText(e.target.value)} onKeyDown={handleKeyDown} placeholder="Type and press Enter" />
             <Box flexWrap="wrap" width="inherit">
-                {tags.map((tag, index) => (
-                    <Button
-                        variant='outlined'
-                        key={index}
-                        onClick={() => handleRemoveTag(index)}
-
-                    >
+                {values.subjects.map((tag, index) => (
+                    <SubjectButton variant='contained' key={index} onClick={() => handleRemoveTag(index)}>
                         {tag}
                         <CloseIcon fontSize='small' />
-                    </Button>
+                    </SubjectButton>
                 ))}
             </Box>
-
-
         </>
     )
 }
--- src/components/TaskDialog.tsx
+++ src/components/TaskDialog.tsx
@@ -5,9 +5,9 @@ import Dialog from '@mui/material/Dialog';
 import { Box, Button, DialogContent, FormLabel, styled, TextField } from '@mui/material';
 import TagsInput from './TagsInput';
 import { Task } from '../types/task';
+import { Form, Formik } from 'formik';
 
-
-export interface SimpleDialogProps {
+export interface TaskDialogProps {
   open: boolean;
   onClose: (value: string) => void;
   task?: Task
@@ -18,73 +18,66 @@ const FormGrid = styled(Box)(() => ({
   flexDirection: 'column',
 }));
 
-const TaskDialog = ({ open, onClose, task }: SimpleDialogProps) => {
-  const [subjects, setSubjects] = useState<string[]>(task ? task.subjects : [])
-  const [description, setDescription] = useState<string | null>(task ? task.description : null)
-  const [priority, setPriority] = useState<number | null>(task ? task.priority : null)
-  const [dueDate, setDueDate] = useState<Date | null>(task ? task.dueDate : null)
-  
-
+const TaskDialog = ({ open, onClose, task }: TaskDialogProps) => {
+  const onSubmit = (values: Task, actions) => {
+    alert(JSON.stringify(values, null, 2));
+    actions.setSubmitting(false);
+  }
   return (
     <>
       <Dialog onClose={onClose} open={open}>
-
         <DialogTitle>{task ? "Edit task" : "Add new task"}</DialogTitle>
         <DialogContent>
+          <Formik initialValues={task || {
+            description: "",
+            priority: null,
+            subjects: [],
+            dueDate: ""
+          }} onSubmit={onSubmit}
+          >
+            {props => (<Form onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+              }
+            }}>
 
-          <Box width="400px" display="flex" flexDirection="column" alignContent="center" gap="15px">
-            <FormGrid >
-              <FormLabel htmlFor="description" required>
-                Task description
-              </FormLabel>
-              <TextField
-                id="description"
-                name="description"
-                type="name"
-                placeholder="Do homework"
-                required
-                size="small"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </FormGrid>
-            <FormGrid >
-              <FormLabel htmlFor="priority" required>
-                Priority
-              </FormLabel>
-              <TextField
-                id="priority"
-                name="priority"
-                type="number"
-                required
-                size="small"
-                value={priority}
-                onChange={(e) => setPriority(Number(e.target.value))}
-              />
-            </FormGrid>
-            <FormGrid >
-              <FormLabel htmlFor="tags" required>
-                Subjects
-              </FormLabel>
+              <Box width="400px" display="flex" flexDirection="column" alignContent="center" gap="15px">
+                <FormGrid >
+                  <FormLabel htmlFor="description" required>
+                    Task description
+                  </FormLabel>
+                  <TextField id="description" name="description" type="name" placeholder="Do homework"
+                    required size="small" value={props.values.description} onChange={props.handleChange}
+                  />
+                </FormGrid>
+                <FormGrid >
+                  <FormLabel htmlFor="priority" required>
+                    Priority
+                  </FormLabel>
+                  <TextField id="priority" name="priority" type="number"
+                    required size="small" value={props.values.priority} onChange={props.handleChange}
+                  />
+                </FormGrid>
+                <FormGrid >
+                  <FormLabel htmlFor="tags" required>
+                    Subjects
+                  </FormLabel>
 
-              <TagsInput tags={subjects} setTags={setSubjects} name="tags"></TagsInput>
-            </FormGrid>
-            <FormGrid >
-              <FormLabel htmlFor="due-date" required>
-                Duo date
-              </FormLabel>
-              <TextField
-                id="due-date"
-                name="due-date"
-                type="date"
-                required
-                size="small"
-                value={dueDate}
-                onChange={(e) => setDueDate(new Date(e.target.value))}
-              />
-            </FormGrid>
-            <Button sx={{ marginTop: "15px" }} variant='outlined' onClick={() => onClose('save')}>{task ? "save changes" : "submit"}</Button>
-          </Box>
+                  <TagsInput></TagsInput>
+                </FormGrid>
+                <FormGrid >
+                  <FormLabel htmlFor="dueDate" required>
+                    Duo date
+                  </FormLabel>
+                  <TextField id="dueDate" name="dueDate" type="date"
+                    required size="small" value={props.values.dueDate} onChange={props.handleChange}
+                  />
+                </FormGrid>
+                <Button type="submit" variant='outlined'>{task ? "save changes" : "submit"}</Button>
+
+              </Box>
+            </Form>)}
+          </Formik>
         </DialogContent>
 
       </Dialog >
