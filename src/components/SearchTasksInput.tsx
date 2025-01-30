e03fa8f7f9b2b53db8db955a5a4adcb6e8512180
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-01-30 15:45:39 +0200

    Add a generic debounce input component

--- src/components/SearchTasksInput.tsx
+++ src/components/SearchTasksInput.tsx
@@ -1,22 +1,23 @@
-import { useContext, useEffect, useState } from 'react'
-import { TextField } from '@mui/material'
+import { useContext } from 'react'
+import { InputAdornment } from '@mui/material'
 import { SearchTaskContext } from '../contexts/SearchTaskContext';
+import DebouncedInput from './DebounceInput';
+import SearchIcon from '@mui/icons-material/Search';
 
 function SearchTasksInput() {
-    const { setSearchValue } = useContext(SearchTaskContext);
-    const [text, setText] = useState('')
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setSearchValue(text)
-        }, 1000)
-
-        return () => clearTimeout(timer)
-    }, [text, setSearchValue])
-
+    const { searchValue, setSearchValue } = useContext(SearchTaskContext);
 
     return (
-        <TextField placeholder='search...' onChange={e => setText(e.target.value)} > </TextField>
+        <DebouncedInput slotProps={{
+            input: {
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <SearchIcon />
+                    </InputAdornment>
+                ),
+            },
+        }} placeholder='search...' value={searchValue} onChange={(value) => setSearchValue(value as string)
+        } />
     )
 }
 
