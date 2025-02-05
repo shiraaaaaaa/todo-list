2834c7a8614fe8504d5626321f4f5f1f54bc7cb0
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-02-05 16:26:56 +0200

    Add useMapHover hook

--- src/components/TasksMap.tsx
+++ src/components/TasksMap.tsx
@@ -1,12 +1,12 @@
-import { useMemo, useRef, useState } from 'react';
-import { Map, Overlay, View } from 'ol';
+import { useMemo } from 'react';
+import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import { Box, Chip } from '@mui/material';
 import StadiaMaps from 'ol/source/StadiaMaps.js';
 import { defaults } from 'ol/control';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-import Feature, { FeatureLike } from 'ol/Feature';
+import Feature from 'ol/Feature';
 import { Point } from 'ol/geom';
 import { fromLonLat } from 'ol/proj';
 import MapLayout from './MapLayout';
@@ -16,14 +16,16 @@ import Select from 'ol/interaction/Select';
 import { Task } from '../types/task';
 import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
-
+import useMapHover from '../hooks/useMapHover';
 
 const TasksMap = ({ onSelect, selectedTask }: { onSelect: (task: Task | null) => void, selectedTask: Task | null }) => {
     const [tasks] = useAtom(tasksAtom)
-    const tooltipRef = useRef<HTMLDivElement | null>(null);
-    const [hoveredFeature, setHoveredFeature] = useState<FeatureLike | null>(null)
 
-    const tasksFeatures = tasks.map(task => new Feature({ geometry: new Point(fromLonLat(task.coordinates)), name: task.description }))
+    const tasksFeatures = useMemo(() => tasks.map(task => new Feature({
+        geometry: new Point(fromLonLat(task.coordinates)),
+        name: task.description,
+        isDone: task.isDone
+    })), [tasks])
 
     const myMap = useMemo(() => new Map({
         controls: defaults({
@@ -39,14 +41,16 @@ const TasksMap = ({ onSelect, selectedTask }: { onSelect: (task: Task | null) =>
                 source: new VectorSource({
                     features: tasksFeatures,
                 }),
-                style: new Style({
+
+                style: (feature) => new Style({
                     image: new Icon({
                         anchor: [0.5, 0.5],
                         opacity: 1,
                         scale: 0.05,
                         anchorXUnits: 'fraction',
                         anchorYUnits: 'pixels',
-                        src: 'src/assets/mapFeaturePoint.png'
+                        src: 'https://cdn-icons-png.flaticon.com/512/1521/1521260.png',
+                        color: feature.get('isDone') ? 'green' : 'yellow'
                     })
                 })
             })
@@ -55,41 +59,29 @@ const TasksMap = ({ onSelect, selectedTask }: { onSelect: (task: Task | null) =>
             center: fromLonLat(tasks[tasks.length - 1].coordinates),
             zoom: 10,
         }),
-    }), [])
+    }), [tasksFeatures])
 
-    const overlay = new Overlay({ element: tooltipRef.current! });
 
-    myMap.addOverlay(overlay);
-
-    myMap.on('pointermove', function (evt) {
-        overlay.setPosition(evt.coordinate);
-        const feature = myMap.getFeaturesAtPixel(evt.pixel)[0]
-
-        if (feature) {
-            setHoveredFeature(feature);
-            tooltipRef.current!.style.visibility = 'visible';
-        } else {
-            tooltipRef.current!.style.visibility = 'hidden';
-        }
-
-    });
-
-    myMap.addInteraction(new Select())
     tasksFeatures.forEach((feature, index) => {
         feature.on('change', () => {
             onSelect(tasks[index] === selectedTask ? null : tasks[index])
         })
     })
 
-    return (
+    myMap.addInteraction(new Select())
 
-        <Box position="relative" height="100%" width="100%" margin='auto'>
-            <MapLayout map={myMap} />
-            <Chip id="info" variant="filled" ref={tooltipRef}
-                label={hoveredFeature ? hoveredFeature.get('name') : ""}
-                sx={{ backgroundColor: "#f8f8f8", padding: "2px" }}
-            />
-        </Box>
+    const [tooltipRef, hoveredFeature] = useMapHover(myMap)
+
+    return (
+        <>
+            <Box position="relative" height="100%" width="100%" margin='auto'>
+                <Chip id="info" variant="filled" ref={tooltipRef}
+                    label={hoveredFeature ? hoveredFeature.get('name') : ""}
+                    sx={{ backgroundColor: "#f8f8f8" }}
+                />
+                <MapLayout map={myMap} />
+            </Box>
+        </>
 
     );
 };
