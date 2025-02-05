343e393de85b5b2138733fb267bfc082e5133d54
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-02-05 17:55:37 +0200

    Remove useless state from tasksList component

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,25 +1,21 @@
-import { useContext, useMemo, useState } from 'react'
+import { useContext, useMemo } from 'react'
 import { Box, Typography } from '@mui/material'
 import TaskCard from '../components/TaskCard'
 import { SearchTaskContext } from '../contexts/SearchTaskContext'
-import { Task } from '../types/task'
 import { useAtom } from 'jotai'
 import { tasksAtom } from '../atoms/tasksAtom'
 
 function TasksList() {
   const [tasks] = useAtom(tasksAtom)
   const { searchValue } = useContext(SearchTaskContext)
-  const [filteredTasks, setFilteredTasks] = useState<Task[]>([])
 
-  useMemo(() => {
-    setFilteredTasks(
-      searchValue == ''
-        ? tasks
-        : tasks.filter((task) =>
-            task.description.toLowerCase().includes(searchValue.toLowerCase()),
-          ),
-    )
-  }, [tasks, searchValue])
+  const filteredTasks = useMemo(() =>
+    searchValue == ''
+      ? tasks
+      : tasks.filter((task) =>
+        task.description.toLowerCase().includes(searchValue.toLowerCase()),
+      )
+    , [tasks, searchValue])
 
   return (
     <Box>
