5177285eff3fe4723ee34c99e49d8ff0b673e3a7
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-02-09 16:23:32 +0200

    Use useMap hook in MapPointInput

--- src/components/MapPointInput.tsx
+++ src/components/MapPointInput.tsx
@@ -1,59 +1,38 @@
 import { useMemo } from 'react'
-import { Map, View } from 'ol'
-import TileLayer from 'ol/layer/Tile'
 import { Box, TextField } from '@mui/material'
-import StadiaMaps from 'ol/source/StadiaMaps.js'
-import { defaults } from 'ol/control'
-import VectorLayer from 'ol/layer/Vector'
-import VectorSource from 'ol/source/Vector'
-import Feature from 'ol/Feature'
-import { Point } from 'ol/geom'
-import { fromLonLat, toLonLat } from 'ol/proj'
 import MapLayout from './MapLayout'
 import { useFormikContext } from 'formik'
 import { Task } from '../types/task'
-import { Coordinate } from 'ol/coordinate'
+import useMap from '../hooks/useMap'
+import { createTileLayer } from '../helpers/map/layers/tileLayer'
+import { createVectorLayer } from '../helpers/map/layers/vectorLayer'
+import { createFeature, updateFeatureGeometry } from '../helpers/map/geometry/Feature'
+import { createGeoJsonOfPoint } from '../helpers/geoJSON'
 
 const MapPointInput = () => {
   const { values, setFieldValue } = useFormikContext<Task>()
+  const displayedFeature = createFeature(createGeoJsonOfPoint(values.coordinates))
 
-  const displayedFeature = new Feature(new Point(fromLonLat(values.coordinates)))
-
-  const myMap = useMemo(
-    () =>
-      new Map({
-        controls: defaults({
-          zoom: false,
-          rotate: false,
-          attribution: false,
-        }),
-        layers: [
-          new TileLayer({
-            source: new StadiaMaps({
-              layer: 'outdoors',
-            }),
-          }),
-          new VectorLayer({
-            source: new VectorSource({
-              features: [displayedFeature],
-            }),
-          }),
-        ],
-        view: new View({
-          center: fromLonLat(values.coordinates),
-          zoom: 10,
-        }),
-      }),
+  const mapOptions = useMemo(
+    () => ({
+      view: { center: values.coordinates, zoom: 10 },
+      layers: [
+        createTileLayer({ type: 'stadia', layer: 'outdoors' }),
+        createVectorLayer([displayedFeature]),
+      ],
+    }),
     [],
   )
 
-  myMap.on('click', (event) => {
-    setCoordinates(toLonLat(event.coordinate))
+  const map = useMap(mapOptions)
+
+  map.on('click', (event) => {
+    setCoordinates(event.coordinate)
   })
 
-  const setCoordinates = (lonLat: Coordinate) => {
+  const setCoordinates = (lonLat: number[]) => {
     setFieldValue('coordinates', lonLat)
-    displayedFeature.setGeometry(new Point(fromLonLat(lonLat)))
+    updateFeatureGeometry(displayedFeature, createGeoJsonOfPoint(lonLat))
   }
 
   return (
@@ -73,7 +52,7 @@ const MapPointInput = () => {
         />
       </Box>
       <Box height={300} width={400}>
-        <MapLayout map={myMap} />
+        <MapLayout map={map} />
       </Box>
     </Box>
   )
