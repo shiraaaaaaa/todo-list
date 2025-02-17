941827e5e73904ecb21b8a0c87a24b44bc64c51c
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-02-17 10:37:56 +0200

    Add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
 import Layout from './Layout'
 import HomePage from './pages/HomePage'
@@ -6,16 +7,20 @@ import TasksPage from './pages/TasksPage'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
 function App() {
+  const queryClient = new QueryClient()
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="list" element={<TasksPage />} />
-          <Route path="manager" element={<ManagerPage />} />
-          <Route path="home" element={<HomePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="list" element={<TasksPage />} />
+            <Route path="manager" element={<ManagerPage />} />
+            <Route path="home" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
