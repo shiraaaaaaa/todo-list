6a497f595eee5562d8798884b83a551dd1fb4ad5
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-02-17 17:45:49 +0200

    Fix hover doesn't work on first load bug

--- src/hooks/map/useMapHover.tsx
+++ src/hooks/map/useMapHover.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Map, Overlay } from 'ol'
 import { FeatureLike } from 'ol/Feature'
 
@@ -6,28 +6,30 @@ const useMapHover = (map: Map) => {
   const [hoveredFeature, setHoveredFeature] = useState<FeatureLike | null>(null)
   const tooltipRef = useRef<HTMLDivElement>(null)
 
-  if (tooltipRef.current) {
-    const overlay = new Overlay({
-      element: tooltipRef!.current,
-      offset: [5, 5],
-    })
+  useEffect(() => {
+    if (tooltipRef.current) {
+      const overlay = new Overlay({
+        element: tooltipRef!.current,
+        offset: [5, 5],
+      })
 
-    map.addOverlay(overlay)
+      map.addOverlay(overlay)
 
-    map.on('pointermove', function (evt) {
-      overlay.setPosition(evt.coordinate)
-      const feature = map.getFeaturesAtPixel(evt.pixel)[0]
+      map.on('pointermove', function (evt) {
+        overlay.setPosition(evt.coordinate)
+        const feature = map.getFeaturesAtPixel(evt.pixel)[0]
 
-      if (feature) {
-        setHoveredFeature(feature)
-        setTimeout(() => {
-          tooltipRef!.current!.style.visibility = 'visible'
-        }, 50)
-      } else {
-        tooltipRef!.current!.style.visibility = 'hidden'
-      }
-    })
-  }
+        if (feature) {
+          setHoveredFeature(feature)
+          setTimeout(() => {
+            tooltipRef!.current!.style.visibility = 'visible'
+          }, 50)
+        } else {
+          tooltipRef!.current!.style.visibility = 'hidden'
+        }
+      })
+    }
+  }, [tooltipRef, map])
 
   return [tooltipRef, hoveredFeature] as const
 }
