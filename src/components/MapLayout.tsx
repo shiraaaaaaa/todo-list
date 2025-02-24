a8fcd9a3cd00f487072c6814e053190c4cdee69c
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-02-24 17:19:21 +0200

    Remove useless ref

--- src/components/MapLayout.tsx
+++ src/components/MapLayout.tsx
@@ -6,21 +6,10 @@ import Map from 'ol/Map'
 
 const MapLayout = ({ map }: { map: Map }) => {
   const mapContainerRef = useRef<HTMLDivElement>(null)
-  const mapInstanceRef = useRef<Map | null>(null)
 
   useEffect(() => {
-    if (!mapContainerRef.current || mapInstanceRef.current) {
-      return
-    }
-    
-    map.setTarget(mapContainerRef.current)
-    mapInstanceRef.current = map
-
-    return () => {
-      if (mapInstanceRef.current) {
-        mapInstanceRef.current.setTarget(undefined)
-        mapInstanceRef.current = null
-      }
+    if (mapContainerRef.current) {
+      map.setTarget(mapContainerRef.current)
     }
   }, [map])
 
