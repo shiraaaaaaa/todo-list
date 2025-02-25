1ad97f073e750a5327858ab29715371a79874b73
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-02-25 15:03:00 +0200

    Fix readability in tasks server functions

--- app/utils/tasks.ts
+++ app/utils/tasks.ts
@@ -5,25 +5,21 @@ import { Task } from '../types/task'
 const tasksUrlPath = '/api/task/'
 
 export const createNewTask = async (task: Omit<Task, '_id'>) => {
-  const res = await axios.post(tasksUrlPath, task)
-  const data = res.data
+  const { data } = await axios.post(tasksUrlPath, task)
   return data
 }
 
 export const updateTask = async ({ id, update }: { id: string; update: Partial<Task> }) => {
-  const res = await axios.put(tasksUrlPath + id, update)
-  const data = res.data
+  const { data } = await axios.put(tasksUrlPath + id, update)
   return data
 }
 
 export const deleteTask = async (id: string) => {
-  const res = await axios.delete(tasksUrlPath + id)
-  const data = res.data
+  const { data } = await axios.delete(tasksUrlPath + id)
   return data
 }
 
 export const getTasks = async (): Promise<Task[]> => {
-  const res = await axios.get(tasksUrlPath)
-  const data = res.data
+  const { data } = await axios.get(tasksUrlPath)
   return data
 }
