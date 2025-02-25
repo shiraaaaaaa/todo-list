14f044edb2f89123342a8d4d74db1ac7ff9a4c09
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-02-25 15:03:50 +0200

    Update vite config to be proxy for backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    ssr: true,
+    rollupOptions: {
+      input: './dist/server/index.js',
+    },
+  },
+  server: {
+    proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          rewrite: (path) => path.replace('/api', ''),
+          changeOrigin: true,
+          configure: (proxy) => {
+            proxy.on('error', (err) => {
+              console.log('proxy error', err);
+            });
+          },
+        }
+      }
+  }
 })
