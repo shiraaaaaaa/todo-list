3a4f309bf7dbf9b099ffc8b78650005c8619d509
ShiraShiloni <SS085060@greendreamteam.onmicrosoft.com>
2025-03-04 11:38:09 +0200

    Update navbar with new hashed routes

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,13 +5,13 @@ const NavBar = () => {
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <Button href="/home" variant="text" color="inherit">
+          <Button href="/#/home" variant="text" color="inherit">
             home
           </Button>
-          <Button href="/list" variant="text" color="inherit">
+          <Button href="/#/list" variant="text" color="inherit">
             list
           </Button>
-          <Button href="/manager" variant="text" color="inherit">
+          <Button href="/#/manager" variant="text" color="inherit">
             manager
           </Button>
         </Toolbar>
